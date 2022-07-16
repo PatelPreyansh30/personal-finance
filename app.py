af2a63ec8bd323a8ee60635410f840ab1ee4a67a
Patel Preyansh <patelpreyansh3011@gmail.com>
2022-07-16 14:04:51 +0530

    Add Local MySQL DataBase

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
-from flask import Flask, redirect,render_template,request
+from flask import Flask, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
-from flask_mysqldb import MySQL
 from flask_security import *
+import json
+
+# Config JSON file
+with open('config.json','r') as c:
+    params = json.load(c)['params']
+local_server = True
+
 
 app = Flask(__name__)
 
 # DataBase Connectivity
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/personal_finance_managment"
+if local_server:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
 db = SQLAlchemy(app)
 
 # Database Schema
-class Signup(db.Model):
-    sno = db.Column(db.Integer, primary_key=True,nullable=False)
-    name = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(60), nullable=False)
-    m_no = db.Column(db.String(20), nullable=False)
-    gender = db.Column(db.String(8), nullable=False)
-    password = db.Column(db.String(30), nullable=False)
-    date = db.Column(db.Date, default=date.today(), nullable=True)
-    
+class Register(db.Model):
+    UserID = db.Column(db.Integer, nullable=False, primary_key=True)
+    Name = db.Column(db.String(25), nullable=False)
+    Email = db.Column(db.String(30), nullable=False)
+    Mno = db.Column(db.String(15), nullable=False)
+    Gender = db.Column(db.String(10), nullable=False)
+    Password = db.Column(db.String(20), nullable=False)
+    Date = db.Column(db.Date, default=date.today(), nullable=False)
+
     def __repr__(self) -> str:
         return f"Record of: {self.email} & {self.name}"
 
 
-@app.route("/",methods=['GET','POST'])
+@app.route("/", methods=['GET', 'POST'])
 def home():
     # if request.method == 'POST':
     #     if request.form.get('credit'):
@@ -55,25 +65,27 @@ def home():
 #     db.session.commit()
 #     return redirect("/")
 
-@app.route("/login",methods=['GET','POST'])
+# Login Page Logic
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-        # print(email,password)
 
         try:
-            query = Signup.query.filter_by(email=email).first()
-            if query.password == password:
-                return render_template("index.html")
+            query = Register.query.filter_by(Email=email).first()
+            if query.Password == password:
+                return redirect("index.html")
             else:
-                return f"Password is Invalid, Please Try Again"
+                return f"<h3>Password is Invalid, Please Try Again </h3>"
         except:
-            return f"Login through valid mail id"
+            return f"<h3>Login through valid mail id </h3>"
 
     return render_template("login.html")
 
-@app.route("/signup",methods=['GET','POST'])
+
+# Signup Page Logic
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         name = request.form.get('name')
@@ -81,21 +93,27 @@ def signup():
         m_no = request.form.get('m_no')
         gender = request.form.get('gender')
         password = request.form.get('password')
-        # print(name,email,m_no,gender,password)
 
-        entry = Signup(name=name,email=email,m_no=m_no,gender=gender,password=password)
-        # print(entry)
+        entry = Register(Name=name, Email=email, Mno=m_no,Gender=gender, Password=password)
         db.session.add(entry)
         db.session.commit()
 
     return render_template("signup.html")
 
-@app.route("/reset",methods=['GET','POST'])
+
+# Reset Page Logic
+@app.route("/reset", methods=['GET', 'POST'])
 def reset():
     if request.method == 'POST':
         email = request.form.get('email')
-        # print(email)
+
+        try:
+            query = Register.query.filter_by(Email=email).first()
+            return query.Password
+        except:
+            return f"<h3>Please Enter Valid Email</h3>"
+
     return render_template("reset.html")
 
 if __name__ == "__main__":
-    app.run(debug=True,threaded=True)
+    app.run(debug=True, threaded=True)

25665e747f8166a271de48c831b049955fba391b
Patel Preyansh <86158122+PatelPreyansh30@users.noreply.github.com>
2022-07-24 15:12:22 +0530

    Add files via upload

---
+++ alerts.py
@@ -0,0 +1,55 @@
+import smtplib
+from email.message import EmailMessage
+from random import random
+
+class EmailAlerts():
+    def email_otp(to):
+        _user = 'abc302266@gmail.com'
+        _password = 'tjhobnvjugvpwjtn'
+
+        otp = int(random()*10000000)
+        body=f"Your One Time Password is: {otp}"
+
+        msg = EmailMessage()
+        msg.set_content(body)
+        msg['subject'] = f"Your One-Time-Password is here"
+        msg['to'] = to
+        msg['from'] = _user
+
+        server = smtplib.SMTP("smtp.gmail.com",587)
+        server.starttls()
+        server.login(_user,_password)
+        server.send_message(msg)
+        server.quit()
+        return otp
+
+    def emailAlerts(to,amount):
+        _user = 'abc302266@gmail.com'
+        _password = 'tjhobnvjugvpwjtn'
+
+        body = f"You have used your {amount} amount, please spend wisely otherwise your monthly budget will loss..."
+
+        msg = EmailMessage()
+        msg.set_content(body)
+        msg['subject'] = f"Your daily budget limit exceeded"
+        msg['to'] = to
+        msg['from'] = _user
+
+        server = smtplib.SMTP("smtp.gmail.com",587)
+        server.starttls()
+        server.login(_user,_password)
+        server.send_message(msg)
+        server.quit()
+
+class CheckFunctions():
+    def email_check( someList, email ):
+        for i in someList:
+            if i[0] == email:
+                return 0
+        return 1
+
+    def mno_check( someList, mno ):
+        for i in someList:
+            if i[0] == mno:
+                return 0
+        return 1
--- app.py
+++ app.py
@@ -1,51 +1,166 @@
-from flask import Flask, redirect,render_template,request
+from flask import *
+from flask_login import *
 from flask_sqlalchemy import SQLAlchemy
-from datetime import date
+from flask_security import *
+from werkzeug.security import *
+from datetime import date,timedelta
+from alerts import EmailAlerts, CheckFunctions
 
+# App Declaration
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///pf.db"
+
+# Flask Security Declaration
+app.secret_key = 'q4t6w9z$C&F)J@NcRfUjXn2r5u8x!A%D*G-KaPdSgVkYp3s6v9y$B?E(H+MbQeThWmZq4t7w!z%C*F)J@NcRfUjXn2r5u8x/A?D(G+KaPdSgVkYp3s6v9y$B&E)H@McQ'
+
+# Declare Session Time-Out
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+# Flask Login Manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# DataBase Connectivity with Development & Production Server (Use PostgreSQL Database in Local or Heroku)
+env = 'dev'
+if env == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Preyansh3011@localhost/personal-finance-management"
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://vwsmfyqhkypfht:d059967f3ec1fd598a60c13e14c4aba3a44ba400b010a2ed82ca3635130568e0@ec2-3-217-14-181.compute-1.amazonaws.com:5432/dasotmoeq12jhn"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
-class PFManage(db.Model):
-    srNo = db.Column(db.Integer,primary_key=True,nullable=False)
-    date = db.Column(db.Date, default=date.today(), nullable=False)
-    credit = db.Column(db.Integer, nullable=False)
-    debit = db.Column(db.Integer, nullable=False)
-    
+# Table Schema (Register)
+class Register(db.Model):
+    __tablename__ = 'register'
+    UserID = db.Column(db.Integer, nullable=False, primary_key=True)
+    Name = db.Column(db.String(30), nullable=False)
+    Email = db.Column(db.String(40), nullable=False,unique=True)
+    Mno = db.Column(db.String(150), nullable=False,unique=True)
+    Gender = db.Column(db.String(10), nullable=False)
+    Age = db.Column(db.Integer, nullable=False)
+    Password = db.Column(db.String(150), nullable=False)
+    Date = db.Column(db.Date, default=date.today(), nullable=False)
+
     def __repr__(self) -> str:
-        return f"Record: {self.date}"
+        return f"Record of: {self.Email} & {self.Name}"
 
+# Table Schema (User Activity)
+
+
+# User Request For App
 @app.route("/",methods=['GET','POST'])
+def index():
+    return render_template("welcome.html")
+
+# Home Page (login required)
+@app.route("/home")
+@login_required
 def home():
+    if 'login_email' and 'login_password' in session:
+        return render_template('home.html')
+    else:
+        return redirect("/login")
+
+# Login Page Logic
+@app.route("/login", methods=['POST','GET'])
+@login_manager.user_loader
+def login():
     if request.method == 'POST':
-        if request.form.get('credit'):
-            credit = request.form['credit']
-            data = PFManage(credit=credit,debit=0)
-            db.session.add(data)
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        session['login_email']=email
+        session['login_password'] = password
+        
+        try:
+            if Register.query.filter_by(Email=email).first().Email:
+                try:
+                    if check_password_hash(Register.query.filter_by(Email=email).first().Password,password):
+                        return redirect("/home")
+                    else:
+                        raise Exception
+                except:
+                    return render_template("/authentication/login.html",valid_pass="Please enter valid password")
+            else:
+                raise Exception
+        except:
+            return render_template("/authentication/login.html",valid_email="Please enter valid email")
+
+    return render_template("/authentication/login.html")
+
+# Signup Page Logic
+@app.route("/signup", methods=['POST','GET'])
+def signup():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        m_no = request.form.get('m_no')
+        gender = request.form.get('gender')
+        age = request.form.get('age')
+        password = generate_password_hash(request.form.get('password'))
+
+        email_ls = Register.query.with_entities(Register.Email).all()
+        mno_ls = Register.query.with_entities(Register.Mno).all()
+        
+        if CheckFunctions.email_check(email_ls,email)==0:
+            return render_template("/authentication/signup.html",email_check="Email is already registered")
+        elif CheckFunctions.mno_check(mno_ls,m_no)==0:
+            return render_template("/authentication/signup.html",mno_check="Mobile Number is already registered")
+        else:
+            entry = Register(Name=name, Email=email, Mno=m_no,Gender=gender,Age=age, Password=password)
+            db.session.add(entry)
             db.session.commit()
-        elif request.form.get('debit'):
-            debit = request.form['debit']
-            data = PFManage(credit=0,debit=debit)
-            db.session.add(data)
+            return render_template("/authentication/signup.html",signupmsg='True')
+    
+    return render_template("/authentication/signup.html")
+
+# Reset Page Logic
+@app.route("/reset", methods=['POST','GET'])
+def reset():
+    if request.method == 'POST':
+        session['reset_email'] = request.form.get('email')
+        try:
+            Register.query.filter_by(Email=session['reset_email']).first().Email
+            session['otp'] = EmailAlerts.email_otp(session['reset_email'])
+            return render_template("/authentication/reset_otp.html",valid_email="Successfully sent the OTP on your email, if you don't recive OTP then check your email's spam folder")
+        except:
+            return render_template("/authentication/reset.html",invalid_email="Please enter valid email")
+
+    return render_template("/authentication/reset.html")
+
+# OTP Verify
+@app.route("/verify",methods=["POST",'GET'])
+def otp():
+    if "reset_email" and "otp" in session:
+        if request.method == "POST":
+            OTP = request.form.get('otp')
+            if str(session['otp']) == OTP:
+                session.pop('otp',None)
+                return render_template("/authentication/new_password.html")
+            else:
+                return render_template("/authentication/reset.html",invalid_otp="Your OTP is incorrect")
+    else:
+        return redirect("/")
+
+# Reset The Password
+@app.route("/set-password",methods=['POST','GET'])
+def set_password():
+    if "reset_email" in session:
+        if request.method=='POST':
+            newPass = generate_password_hash(request.form.get('new-password'))
+            old_data = Register.query.filter_by(Email=session['reset_email']).first()
+            old_data.Password = newPass
             db.session.commit()
+            session.clear()
+            return render_template("/authentication/new_password.html",reset_password_done="True")
+        else:
+            session.clear()
+            return redirect('/login')
+    else:
+        return redirect("/")
 
-    allData = PFManage.query.all()
-    return render_template("index.html",allData = allData)
-
-@app.route("/delete/int:<srNo>")
-def delete(srNo):
-    data = PFManage.query.filter_by(srNo=srNo).first()
-    db.session.delete(data)
-    db.session.commit()
-    return redirect("/")
-
-@app.route("/delete")
-def deleteAll():
-    data = PFManage.query.all()
-    db.session.query(PFManage).delete()
-    db.session.commit()
-    return redirect("/")
-
+# Running the App
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
